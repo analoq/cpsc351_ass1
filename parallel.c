6220834fb34b818e19cb146690079c91958ccd59
A. P. Matthews <apmatthews@csu.fullerton.edu>
2017-07-22 11:26:38 -0700

    add error checking

--- parallel.c
+++ parallel.c
@@ -10,15 +10,27 @@
 int main(int argc, char *argv[])
 {
   char line[128];
-  int count;
+  int pid, count;
 
   FILE *fp = fopen("urls.txt", "rt");
   for ( count = 0; fgets(line, 128, fp); count ++ )
   {
     if ( line[strlen(line)-1] == '\n')
       line[strlen(line)-1] = 0;
-    if ( !fork() )
-      execlp("/usr/bin/wget", "wget", line, NULL);
+    pid = fork();
+    if ( pid < 0 )
+    {
+      fprintf(stderr, "Unable to fork!\n");
+      return -1;
+    }
+    if ( !pid )
+    {
+      if ( execlp("/usr/bin/wget", "wget", line, NULL) < 0 )
+      {
+        fprintf(stderr, "Unable to exec!\n");
+        return -1;
+      }
+    }
   }
   fclose(fp);
 
--- serial.c
+++ serial.c
@@ -10,17 +10,30 @@
 int main(int argc, char *argv[])
 {
   char line[128];
-  int count;
+  int pid;
 
   FILE *fp = fopen("urls.txt", "rt");
-  for ( count = 0; fgets(line, 128, fp); count ++ )
+  while ( fgets(line, 128, fp) )
   {
     if ( line[strlen(line)-1] == '\n')
       line[strlen(line)-1] = 0;
-    if ( !fork() )
-      execlp("/usr/bin/wget", "wget", line, NULL);
-    else
+
+    pid = fork();
+    if ( pid < 0 )
+    {
+      fprintf(stderr, "Unable to fork!\n");
+      return -1;
+    }
+    if ( pid )
       wait(NULL);
+    else
+    {
+      if ( execlp("/usr/bin/wget", "wget", line, NULL) < 0 )
+      {
+        fprintf(stderr, "Unable to exec!\n");
+        return -1;
+      }
+    }
   }
   fclose(fp);
 
